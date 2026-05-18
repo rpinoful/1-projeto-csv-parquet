d1bc0fa3792c7e460a51e27c2b1279f92d9bee2b
rpinoful <rapsful@gmail.com>
2026-05-18 16:26:14 -0300

    criando a função extract

--- etl.py
+++ etl.py
@@ -2,11 +2,23 @@ import os
 import glob
 import pandas as pd
 
+# funções:
+# 1 - Função que le e consolida o dataframe
+# 2 - Função que transforma
+# 3 - Função que carrega e salva no formato requerido
+
+
 pasta = 'data'
-arquivos_json = glob.glob(os.path.join(pasta,'*.json'))
+def extract(pasta:str)-> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta,'*.json'))
+    #Reading dataframes
+    dataframes = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    #concating dataframes
+    df_concat = pd.concat(dataframes,ignore_index=True)
+    print(df_concat.to_markdown())
+    return df_concat
 
 
-#Reading dataframes
-dataframes = [pd.read_json(arquivo) for arquivo in arquivos_json]
-df_concat = pd.concat(dataframes,ignore_index=True)
-print(df_concat.to_markdown())
+
+
+
